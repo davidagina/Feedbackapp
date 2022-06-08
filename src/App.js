a813eb7be1e0d7d5254a7155390fe9fafd5b3ced
davidagina <davidagina64@gmail.com>
2022-06-08 14:08:11 +0100

    Feedback Stats Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from 'react'
+import FeedbackList from "./components/FeedbackList"
+import Header from "./components/Header"
+import FeedbackData from './components/data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure, you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+        
+    }
+
     return (
+        <>
+        <Header />
         <div className="container">
-        </div>
+            <FeedbackStats feedback={feedback}/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        </div> 
+        </>
+        
     )
 }
 
---
+++ src/components/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum sit amet consectectuer'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'Lorem ipsum sit amet consectectuer'
+    },
+    {
+        id: 3,
+        rating: 3,
+        text: 'Lorem ipsum sit amet consectectuer'
+    }
+]
+
+export default FeedbackData
